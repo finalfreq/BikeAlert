0859eb43f588581f2737536d0a693db7bb698e5c
james williams & chase reckling <student@epicodus.com>
2015-06-24 11:55:58 -0700

    added option to add bike to individual user page

--- app/models/bike.js
+++ app/models/bike.js
@@ -2,9 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   color: DS.attr('string'),
-  model: DS.attr('string'),
+  make: DS.attr('string'),
   serial: DS.attr('string'),
   location: DS.attr('string'),
   date: DS.attr('date'),
-  user: DS.belongsTo('user')
+  lost: DS.attr('boolean'),
+  user: DS.belongsTo('user', { async: true })
 });
--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   phone: DS.attr('string'),
-  bikes: DS.hasMany('bike')
+  bikes: DS.hasMany('bike', { async: true })
 });
